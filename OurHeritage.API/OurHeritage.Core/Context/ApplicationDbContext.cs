ec755a43bfc6c75253074684b0ba98e644be8338
Mahmoud Elaaser <mahmoudelaaser57@gmail.com>
2025-02-04 17:22:26 +0200

    Implement Authentication & Authorization features

---
+++ OurHeritage.API/OurHeritage.Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OurHeritage.Core.Entities;
+using System.Reflection;
+
+namespace OurHeritage.Core.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            foreach (var relationship in modelBuilder.Model
+                .GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ OurHeritage.API/OurHeritage.Service/ModuleServiceDependencies.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OurHeritage.Service.Implementations;
+using OurHeritage.Service.Interfaces;
+using System.Reflection;
+
+namespace OurHeritage.Service
+{
+    public static class ModuleServiceDependencies
+    {
+        public static void AddServiceDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddMemoryCache();
+
+
+            /// Register automapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+    }
+}
